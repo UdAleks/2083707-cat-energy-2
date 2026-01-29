015ca3f7fe134f0d393efdc67b605b7079855964
UdAleks <96179276+UdAleks@users.noreply.github.com>
2026-01-29 17:24:34 +0600

    Доработка оптимизации background-image. Доработка index.js. Начало подгонки под PixelPrefect.

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -5,6 +5,8 @@ const mainNavToggle = document.querySelector('.main-nav__toggle');
 const siteNavList = document.querySelector('.site-nav-list');
 
 mainHeader.classList.remove('main-header--nojs');
+mainNavList.classList.toggle('main-nav--opened');
+siteNavList.classList.toggle('site-nav-list--closed');
 
 mainNavToggle.addEventListener('click', () => {
   mainNavList.classList.toggle('main-nav--opened');
